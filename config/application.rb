e90564a51bf68dbb9c7fba1983e950b7651b6ce6
Wilfried Canirinka <canirinkawilfried@gmail.com>
2020-06-09 16:23:17 +0200

    Add Config to the API

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,15 @@ module TrainersApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
+      allow do
+      origins '*'
+      resource(
+      '*',
+      headers: :any,
+      methods: [:get, :patch, :put, :delete, :post, :options]
+      )
+      end
+    end 
   end
 end
