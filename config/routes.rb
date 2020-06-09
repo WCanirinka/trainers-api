e4248efcc6f9747e2d13813f3f7209eb45908954
Wilfried Canirinka <canirinkawilfried@gmail.com>
2020-06-09 18:09:31 +0200

    Add path to login

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@
 
 Rails.application.routes.draw do
   root 'static#home'
-  get 'login', to: 'user#check'
+  get 'login', to: 'users#check'
   resources :users
   resources :gym_instructors
   resources :gym_classes
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
