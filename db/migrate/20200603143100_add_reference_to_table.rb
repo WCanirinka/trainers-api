9a9bc6d0feb476421bc90cb83cc6ae37cc5ce0d0
Wilfried Canirinka <canirinkawilfried@gmail.com>
2020-06-12 01:21:28 +0200

    Alter the db for user and instructors

--- db/migrate/20200603143100_add_reference_to_table.rb
+++ db/migrate/20200603143100_add_reference_to_table.rb
@@ -1,6 +1,6 @@
 class AddReferenceToTable < ActiveRecord::Migration[6.0]
   def change
-    add_reference :gym_classes, :gym_instructors, foreign_key: true
-    add_reference :gym_classes, :users, foreign_key: true
+    add_reference :gym_classes, :gym_instructor, foreign_key: true
+    add_reference :gym_classes, :user, foreign_key: true
   end
 end
--- db/migrate/20200604171145_fix_column_name.rb
+++ db/migrate/20200604171145_fix_column_name.rb
@@ -1,9 +1,5 @@
 class FixColumnName < ActiveRecord::Migration[6.0]
   def change
     rename_column :gym_instructors, :instructorName, :gymName
-    remove_column :gym_classes, :instructor
-    remove_column :gym_classes, :user
-    rename_column :gym_classes, :users_id, :user_id
-    rename_column :gym_classes, :gym_instructors_id, :gym_instructor_id
   end
 end
