92736305aa4a5b09af7c72d319ddf67c85290891
Wilfried Canirinka <canirinkawilfried@gmail.com>
2020-06-12 10:54:16 +0200

    Update seed in the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,57 +1,57 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
-GymInstructor.create!(
-  name: 'James',
-  instImage: 'https://raw.githubusercontent.com/WCanirinka/trainers-api/develop/images/chest-gym-instructor.jpg',
-  gymName: 'Chest Gym',
-  trainingType: 'Chest Training',
-  gymImage: 'https://raw.githubusercontent.com/WCanirinka/trainers-api/develop/images/chest-gym.jpg'
-)
+# GymInstructor.create!(
+#   name: 'James',
+#   instImage: 'https://raw.githubusercontent.com/WCanirinka/trainers-api/develop/images/chest-gym-instructor.jpg',
+#   gymName: 'Chest Gym',
+#   trainingType: 'Chest Training',
+#   gymImage: 'https://raw.githubusercontent.com/WCanirinka/trainers-api/develop/images/chest-gym.jpg'
+# )
 
-GymInstructor.create!(
-  name: 'Roosevelt',
-  instImage: 'https://raw.githubusercontent.com/WCanirinka/trainers-api/develop/images/leg-workout-instructor.png',
-  gymName: 'Leg Gym',
-  trainingType: 'Legs Workout',
-  gymImage: 'https://raw.githubusercontent.com/WCanirinka/trainers-api/develop/images/leg-gym.jpg'
-)
+# GymInstructor.create!(
+#   name: 'Roosevelt',
+#   instImage: 'https://raw.githubusercontent.com/WCanirinka/trainers-api/develop/images/leg-workout-instructor.png',
+#   gymName: 'Leg Gym',
+#   trainingType: 'Legs Workout',
+#   gymImage: 'https://raw.githubusercontent.com/WCanirinka/trainers-api/develop/images/leg-gym.jpg'
+# )
 
-GymInstructor.create!(
-  name: 'Mani',
-  instImage: 'https://raw.githubusercontent.com/WCanirinka/trainers-api/develop/images/arms-gym-instructor.jpg',
-  gymName: 'Arms Gym',
-  trainingType: 'Arms Training',
-  gymImage: 'https://raw.githubusercontent.com/WCanirinka/trainers-api/develop/images/arms-gym.jpg'
-)
+# GymInstructor.create!(
+#   name: 'Mani',
+#   instImage: 'https://raw.githubusercontent.com/WCanirinka/trainers-api/develop/images/arms-gym-instructor.jpg',
+#   gymName: 'Arms Gym',
+#   trainingType: 'Arms Training',
+#   gymImage: 'https://raw.githubusercontent.com/WCanirinka/trainers-api/develop/images/arms-gym.jpg'
+# )
 
-GymInstructor.create!(
-  name: 'Jasmine',
-  instImage: 'https://raw.githubusercontent.com/WCanirinka/trainers-api/develop/images/abs-gym-instructor.jpg',
-  gymName: 'Abs Gym',
-  trainingType: 'Abs Training',
-  gymImage: 'https://raw.githubusercontent.com/WCanirinka/trainers-api/develop/images/abs-gym.jpg'
-)
+# GymInstructor.create!(
+#   name: 'Jasmine',
+#   instImage: 'https://raw.githubusercontent.com/WCanirinka/trainers-api/develop/images/abs-gym-instructor.jpg',
+#   gymName: 'Abs Gym',
+#   trainingType: 'Abs Training',
+#   gymImage: 'https://raw.githubusercontent.com/WCanirinka/trainers-api/develop/images/abs-gym.jpg'
+# )
 
-GymInstructor.create!(
-  name: 'Natalie',
-  instImage: 'https://raw.githubusercontent.com/WCanirinka/trainers-api/develop/images/calf-gym-instructor.png',
-  gymName: 'Calf Gym',
-  trainingType: 'Calves Training',
-  gymImage: 'https://raw.githubusercontent.com/WCanirinka/trainers-api/develop/images/calf-gym.jpg'
-)
+# GymInstructor.create!(
+#   name: 'Natalie',
+#   instImage: 'https://raw.githubusercontent.com/WCanirinka/trainers-api/develop/images/calf-gym-instructor.png',
+#   gymName: 'Calf Gym',
+#   trainingType: 'Calves Training',
+#   gymImage: 'https://raw.githubusercontent.com/WCanirinka/trainers-api/develop/images/calf-gym.jpg'
+# )
 
-GymInstructor.create!(
-  name: 'William',
-  instImage: 'https://raw.githubusercontent.com/WCanirinka/trainers-api/develop/images/cardio-gym-instructor.png',
-  gymName: 'Cardio Gym',
-  trainingType: 'Cardio Training',
-  gymImage: 'https://raw.githubusercontent.com/WCanirinka/trainers-api/develop/images/cardio-gym.png'
-)
+# GymInstructor.create!(
+#   name: 'William',
+#   instImage: 'https://raw.githubusercontent.com/WCanirinka/trainers-api/develop/images/cardio-gym-instructor.png',
+#   gymName: 'Cardio Gym',
+#   trainingType: 'Cardio Training',
+#   gymImage: 'https://raw.githubusercontent.com/WCanirinka/trainers-api/develop/images/cardio-gym.png'
+# )
 
-GymInstructor.create!(
-  name: 'Desmond',
-  instImage: 'https://raw.githubusercontent.com/WCanirinka/trainers-api/develop/images/shoulders-gym-instructor.jpg',
-  gymName: 'Shoulders Gym',
-  trainingType: 'Shoulders Training',
-  gymImage: 'https://raw.githubusercontent.com/WCanirinka/trainers-api/develop/images/shoulder-gym.jpg'
-)
+# GymInstructor.create!(
+#   name: 'Desmond',
+#   instImage: 'https://raw.githubusercontent.com/WCanirinka/trainers-api/develop/images/shoulders-gym-instructor.jpg',
+#   gymName: 'Shoulders Gym',
+#   trainingType: 'Shoulders Training',
+#   gymImage: 'https://raw.githubusercontent.com/WCanirinka/trainers-api/develop/images/shoulder-gym.jpg'
+# )
