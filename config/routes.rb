0c2a6a6cee8f601bffbd2dfbd62bb81b29cd3a94
Wilfried Canirinka <canirinkawilfried@gmail.com>
2020-10-26 14:46:55 +0200

    Revert "Develop"

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
-  root 'static#home'
-  get 'login', to: 'users#check'
-  resources :users
-  resources :gym_instructors
-  resources :gym_classes
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- spec/factories/gym_classes.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :gym_class do
-    user_id { 1 }
-    gym_instructor { 1 }
-    classTime { '2020-06-06' }
-    status { false }
-  end
-end
